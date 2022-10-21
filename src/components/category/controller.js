c7c9481cab0fc352867d160ddf0ec42def71b270
MAC31 <mac31@MacBook-Pro-de-MAC31.local>
2022-10-21 11:15:35 -0500

    Primer commit: Primera parte Reto - Back BSALE

---
+++ src/components/category/controller.js
@@ -0,0 +1,61 @@
+import prisma from "../../db"
+import { success, failed } from "../../responses"
+
+export const findAll = async (req, res) => {
+    try{
+        const categories = await prisma.category.findMany()
+        return success({ res, data: categories})
+    }catch(error){
+        return failed({ res, error})
+    }
+}
+
+export const findOne = async (req, res) => {
+    try{
+        const id = Number(req.params)
+        const category = await prisma.category.findUnique({
+            where: { id },
+        })
+        return success({ res, data:category })
+    }catch(error){
+        return failed({ res, error });
+    }
+}
+
+export const create = async (req, res) => {
+    try{
+        const body = req.body
+        const category = await prisma.category.create({
+            data: body,
+        })
+        return success({ res, data:category , status:201 })
+    }catch(error){
+        return failed({ res, error })
+    }
+}
+
+export const update = async (req, res) => {
+    try{
+        const body = req.body
+        const id = Number(req.params)
+        const category = await prisma.category.update({
+            where: { id },
+            data: body,
+        })
+        return success({ res, data:category})
+    }catch(error){
+        return failed({ res, error })
+    }
+}
+
+export const remove = async (req, res) => {
+    try{
+        const id = Number(req.params)
+        const category = await prisma.category.delete({
+            where: { id },
+        })
+        return success({ res, data:category })
+    }catch(error){
+        return failed({ res, error })
+    }
+}
